8eb7443e2eed30746007fc0378b9326f350229a5
Sam-79 <sameerborkar79@gmail.com>
2022-05-26 14:28:50 +0530

    services section added

--- JS/main.js
+++ JS/main.js
@@ -1,11 +1,15 @@
 // to change header color on scroll
-window.onscroll = (e) => {
-    if (window.scrollY > 150) {
+window.onscroll = function(){changeNavColor()}
+
+
+//function to change background color of navigation bar
+function changeNavColor() {
+    if (window.scrollY > 100) {
         document.querySelector("#navBar").classList.add("navColorChange");
         //  document.querySelector("#navBar").classList.add("navbar-dark");
     }
-    if (window.scrollY < 150) {
+    if (window.scrollY < 100) {
         document.querySelector("#navBar").classList.remove("navColorChange");
         //  document.querySelector("#navBar").classList.remove("navbar-dark");
     }
-};
+}
